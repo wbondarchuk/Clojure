8ff7a8f52102ba1e7c76ffc9c4057427b39b5fb0
wbondarchuk <wbondarchuk@gmail.com>
2022-10-01 21:00:12 +0700

    Tail recur

--- task1/src/task1/core.clj
+++ task1/src/task1/core.clj
@@ -1,128 +1,42 @@
 (ns task1.core)
 
-;(defn foo
-;  "I don't do a whole lot."
-;  [x]
-;  (println x "Hello, World!"))
-;
-;(defn square [x]
-;  (* x x))
-;
-;(defn hypot [x y]
-;  (Math/sqrt (+ (square x) (square y))))
-;
-;(def mult2 (fn [x] (+ x x)))
-;
-;(defn twice [f] (fn [x] (f (f x))))
-;;((twice square) 4)
-;
-;(defn fact [n]
-;  (if (zero? n)
-;    1
-;    (* n (fact (dec n)))))
-;
-;(defn fact-tail [n acc]
-;  (if (zero? n)
-;    acc
-;    (fact-tail (dec n) (* acc n))))
-;
-;(defn fact-right
-;  ([n] (fact-right n 1))
-;  ([n acc]
-;   (if (zero? n)
-;     acc
-;     (fact-tail (dec n) (* acc n)))))
-;
-;(defn fact2 [n]
-;  (let [anon-fact
-;        (fn [n acc]
-;          (if (zero? n)
-;            acc
-;            (recur (dec n) (* acc n))))]
-;    (anon-fact n 1N)))
-;(defn fib [n]
-;  (cond
-;    (== 0 n) 1
-;    (== 1 n) 1
-;    :else (+ (fib (- n 1)) (fib (- n 2)))))
-;(mapv fib (range 10))
+;Использую возведение в степень, чтобы найти всевозможные комбинации
+(defn pow [x n]
+  (loop [acc 1
+         n n]
+    (if (<= n 0) acc
+                 (recur (* x acc) (dec n)))))
 
-;(def fib
-;  (memoize
-;    (fn [n]
-;      (cond
-;        (== 0 n) 1N
-;        (== 1 n) 1N
-;        :else (+ (fib (- n 1)) (fib (- n 2)))))))
-;
-;(defn iter-fib' [n a b]
-;  (if (== 0 n)
-;    a
-;    (recur (dec n) b (+ a b))))
+;Получаю индекс следующей буквы
+(defn get_letr [letr_count words_count i pos prev_index prev_div]
+  (let [first_letr? (= pos 0)
+        div (if first_letr? (quot words_count letr_count)
+                            (quot prev_div (dec letr_count)))
+        rest (if first_letr? letr_count
+                             (dec letr_count))
+        letr_index (mod (quot i div) rest)
+        offset? (<= prev_index letr_index)
+        new_index (+ letr_index (if offset? 1 0))]
+    (list div new_index)))
 
-;(defn iter-fib [n]
-;  (iter-fib' n 1N 1N))
-
-;(defn iter-fib [n]
-;  (letfn [
-;          (iter-fib- [n a b]
-;            (if (== 0 n)
-;              a
-;              (iter-fib- (dec n) b (+ a b))))
-;          ]
-;    (iter-fib- n 1N 1N)))
-
-
-;---- 1.1 ----
-;(defn getAlphabet [alphabet n]
-;  (if (or (zero? n) (empty? alphabet))
-;    (println "No combinations!")
-;    (println n alphabet)))
-;
-;(defn permutations [alphabet]
-;  (cond
-;    (= (count alphabet) 1) (list alphabet)
-;    :else (apply concat
-;                 (map (fn [head]
-;                      (map (fn [tail]
-;                             (cons head tail))
-;                           (permutations (remove #{head} alphabet))))
-;                      alphabet))))
-
-
-;(defn permute
-;  ([alphabet n] (permute alphabet n nil))
-;  ([alphabet n prev]
-;   (cond
-;     (or (zero? n) (empty? alphabet)) (println "No combinations!")
-;     :else (cons (first alphabet) (permute alphabet (dec n) (first alphabet))))
-;   ))
-
-;Выше эксперементы, ниже решение
-
-;Вывод в виде строк
-(defn permute_add_end [prefix alphabet num_of_items]
-  (if (zero? num_of_items)
-    nil
-    (if (= (nth alphabet (dec num_of_items)) (str (last prefix)))
-      (permute_add_end prefix alphabet (dec num_of_items))
-      (concat
-        (conj
-          (permute_add_end prefix alphabet (dec num_of_items))
-          (str prefix (nth alphabet (dec num_of_items))))))))
-
-(defn permute_add_list [list alphabet num_of_items counter]
-  (if (= counter (count list))
-    (if (nil? list)
-      (permute_add_end nil alphabet num_of_items)
-      nil)
-    (concat
-      (permute_add_end (nth list counter) alphabet num_of_items)
-      (permute_add_list list alphabet num_of_items (inc counter)))))
 
 (defn permute [alphabet length]
-  (if (or (zero? length) (empty? alphabet))
-    nil
-    (permute_add_list (permute alphabet (dec length)) alphabet (count alphabet) 0)))
-
+  (let [letters (vec (set alphabet))
+        letr_count (count letters)
+        init []
+        words_count (* letr_count (pow (dec letr_count) (dec length)))]
+    (loop [word init
+           i (dec words_count)
+           pos 0
+           res '()
+           prev_index letr_count
+           prev_div nil]
+      (cond
+        (or (neg? length) (neg? i)) res
+        (= pos length) (recur init (dec i) 0 (conj res word) letr_count nil)
+        :else (let [build (get_letr letr_count words_count i pos prev_index prev_div)
+                    div (first build)
+                    letr_index (second build)
+                    new_word (conj word (get letters letr_index))]
+                (recur new_word i (inc pos) res letr_index div))))))
 
--- task1/test/task1/core_test.clj
+++ task1/test/task1/core_test.clj
@@ -14,12 +14,36 @@
 
 (deftest empty-test-task1-permute
   (testing "Empty alphabet"
-    (is (= (permute [] 2) nil))))
+    (is (= (permute [] 2) ()))))
 
 (deftest zero-test-task1-permute
   (testing "Zero length"
-    (is (= (permute ["a" "b" "c"] 0) nil))))
+    (is (= (permute ["a" "b" "c"] 0) ()))))
+
+(deftest negative-test-task1-permute
+  (testing "Negative length"
+    (is (= (permute ["a" "b" "c"] -2) ()))))
 
 (deftest given-test-task1-permute
   (testing "Тест из задачи"
-    (is (= (permute ["a" "b" "c"] 2) ["cb" "ca" "bc" "ba" "ac" "ab"]))))
+    (is (= (permute '("a" "b" "c") 2) '(["a" "b"] ["a" "c"] ["b" "a"] ["b" "c"] ["c" "a"] ["c" "b"])))))
+
+(deftest test-cons1-task1-permute
+  (testing "'(a) 2"
+    (is (= (permute '(a) 2) ()))))
+
+(deftest test-cons2-task1-permute
+  (testing "'(a a) 2"
+    (is (= (permute '(a a) 2) ()))))
+
+(deftest test-cons3-task1-permute
+  (testing "'(a b) 2"
+    (is (= (permute '(a b) 2) '([a b] [b a])))))
+
+(deftest test-num-vec-task1-permute
+  (testing "Вектор с числами"
+    (is (= (permute [3 2 1] 3) '([1 3 1] [1 3 2] [1 2 1] [1 2 3] [3 1 3] [3 1 2] [3 2 1] [3 2 3] [2 1 3] [2 1 2] [2 3 1] [2 3 2])))))
+
+(deftest test-number-task1-permute
+  (testing "Работа с числами"
+    (is (= (permute [32 1] 2) '([1 32] [32 1])))))
