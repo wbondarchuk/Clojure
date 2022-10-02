dc807f3548257aa25ac4d2d31cc0ff76b76a062e
wbondarchuk <wbondarchuk@gmail.com>
2022-10-02 19:17:13 +0700

    1.3 and 1.4

--- task1/src/task1/core.clj
+++ task1/src/task1/core.clj
@@ -32,11 +32,57 @@
            prev_index letr_count
            prev_div nil]
       (cond
-        (or (neg? length) (neg? i)) res
+        (or (<= length 0) (neg? i)) res
         (= pos length) (recur init (dec i) 0 (conj res word) letr_count nil)
-        :else (let [build (get_letr letr_count words_count i pos prev_index prev_div)
-                    div (first build)
-                    letr_index (second build)
+        :else (let [result (get_letr letr_count words_count i pos prev_index prev_div)
+                    div (first result)
+                    letr_index (second result)
                     new_word (conj word (get letters letr_index))]
                 (recur new_word i (inc pos) res letr_index div))))))
 
+;1.3
+(defn my-map [f coll]
+  (reduce (fn [acc item]
+            (cons item acc)) ; reverse
+    '()
+    (reduce (fn [acc item]
+              (cons (f item) acc))
+      '()
+            coll)))
+
+
+(defn my-filter [pred coll]
+  (reduce (fn [acc item]
+            (cons item acc)) ; reverse
+    '()
+    (reduce (fn [acc item]
+              (if (pred item) (cons item acc)
+                           acc))
+      '()
+            coll)))
+
+
+;1.4
+(defn permute2 [alphabet length]
+  (if (<= length 0) ()
+                    (let [contains (fn [item l]
+                                     (reduce (fn [acc cur]
+                                               (if (= item cur) true
+                                                                acc))
+                                             false l))
+                          builder (fn [oldList] (reduce (fn [result item]
+                                                          (concat item result))
+                                                        ()
+                                                        (map (fn [oldWord]
+                                                               (reduce (fn [acc letter]
+                                                                         (if (= letter (first oldWord)) acc
+                                                                                                        (cons (cons letter oldWord) acc)))
+                                                                       ()
+                                                                       alphabet))
+                                                             oldList)))]
+                      (map (fn [x]
+                             (apply str x)) ;(a b c) => abc
+                           (reduce (fn [acc i]
+                                     (builder acc))
+                                   '(())
+                                   (range length))))))
--- task1/test/task1/core_test.clj
+++ task1/test/task1/core_test.clj
@@ -2,16 +2,7 @@
   (:require [clojure.test :refer :all]
             [task1.core :refer :all]))
 
-;(deftest a-test
-;  (testing "FIXME, I fail."
-;    (is (= 0 1))))
-;
-;(deftest test-fact
-;  (testing "n == 0"
-;    (is (= (fact-right 0) 1)))
-;  (testing "n == 1"
-;    (is (= (fact-right 1) 1))))
-
+;;Тесты для задачи 1.1 и 1.2
 (deftest empty-test-task1-permute
   (testing "Empty alphabet"
     (is (= (permute [] 2) ()))))
@@ -47,3 +38,58 @@
 (deftest test-number-task1-permute
   (testing "Работа с числами"
     (is (= (permute [32 1] 2) '([1 32] [32 1])))))
+
+;;Тесты для 1.3
+(deftest test-range6-task1-my-map
+  (testing "range 6"
+    (is (= (println (map (fn [x] (* x x)) (range 6))) (println (my-map (fn [x] (* x x)) (range 6)))))))
+
+(deftest test-inc-task1-my-map
+  (testing "inc"
+    (is (= (println (map inc [1 2 3 4 5])) (println (my-map inc [1 2 3 4 5]))))))
+
+(deftest test-range6-even-task1-my-filter
+  (testing "Четные числа range 6"
+    (is (= (println (filter even? (range 6))) (println (my-filter even? (range 6)))))))
+
+(deftest test-range121-mod3-task1-my-filter
+  (testing "Из лекции"
+    (is (= (println (filter (fn [n] (= 0 (mod n 3))) (range 1 21))) (println (my-filter (fn [n] (= 0 (mod n 3))) (range 1 21)))))))
+
+;Тесты для задачи 1.4
+(deftest empty-test-task1-permute2
+  (testing "Empty alphabet"
+    (is (= (permute [] 2) ()))))
+
+(deftest zero-test-task1-permute2
+  (testing "Zero length"
+    (is (= (permute2 ["a" "b" "c"] 0) ()))))
+
+(deftest negative-test-task1-permute2
+  (testing "Negative length"
+    (is (= (permute2 ["a" "b" "c"] -2) ()))))
+
+(deftest given-test-task1-permute2
+  (testing "Тест из задачи"
+    (is (= (permute2 '("a" "b" "c") 2) '("ca" "ba" "cb" "ab" "bc" "ac")))))
+
+(deftest test-cons1-task1-permute2
+  (testing "'(a) 2"
+    (is (= (permute2 '(a) 2) ()))))
+
+(deftest test-cons2-task1-permute2
+  (testing "'(a a) 2"
+    (is (= (permute2 '(a a) 2) ()))))
+
+(deftest test-cons3-task1-permute2
+  (testing "'(a b) 2"
+    (is (= (permute2 '(a b) 2) '("ba" "ab")))))
+
+(deftest test-num-vec-task1-permute2
+  (testing "Вектор с числами"
+    (is (= (permute2 [3 2 1] 3) '("131" "231" "121" "321" "132" "232" "212" "312" "123" "323" "213" "313")))))
+
+(deftest test-number-task1-permute2
+  (testing "Работа с числами"
+    (is (= (permute2 [32 1] 2) '("132" "321")))))
+
