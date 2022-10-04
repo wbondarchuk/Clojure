5e3de7dc32b3fb44239f21cefbcf2867797937c2
wbondarchuk <wbondarchuk@gmail.com>
2022-10-04 21:54:58 +0700

    Fix for 1.2 and 1.4

--- task1/src/task1/core.clj
+++ task1/src/task1/core.clj
@@ -1,88 +1,60 @@
 (ns task1.core)
 
-;Использую возведение в степень, чтобы найти всевозможные комбинации
-(defn pow [x n]
-  (loop [acc 1
-         n n]
-    (if (<= n 0) acc
-                 (recur (* x acc) (dec n)))))
+;1.2
+(defn compareAndAdd [inList word outList]
+  (if (= (first inList) nil)
+    outList
+    (if (= (first inList) word)
+      (recur (rest inList) word outList)
+      (recur (rest inList) word (cons (str (first inList) word) outList)))))
 
-;Получаю индекс следующей буквы
-(defn get_letr [letr_count words_count i pos prev_index prev_div]
-  (let [first_letr? (= pos 0)
-        div (if first_letr? (quot words_count letr_count)
-                            (quot prev_div (dec letr_count)))
-        rest (if first_letr? letr_count
-                             (dec letr_count))
-        letr_index (mod (quot i div) rest)
-        offset? (<= prev_index letr_index)
-        new_index (+ letr_index (if offset? 1 0))]
-    (list div new_index)))
+(defn nextWord [inList word outList]
+  (if (= (first word) nil)
+    outList
+    (recur inList (rest word) (concat (compareAndAdd inList (first word) nil) outList))))
 
+(defn permuteAll [inList outList length]
+  (if (= length 1)
+    outList
+    (recur inList (nextWord inList outList '()) (dec length))))
 
 (defn permute [alphabet length]
-  (let [letters (vec (set alphabet))
-        letr_count (count letters)
-        init []
-        words_count (* letr_count (pow (dec letr_count) (dec length)))]
-    (loop [word init
-           i (dec words_count)
-           pos 0
-           res '()
-           prev_index letr_count
-           prev_div nil]
-      (cond
-        (or (<= length 0) (neg? i)) res
-        (= pos length) (recur init (dec i) 0 (conj res word) letr_count nil)
-        :else (let [result (get_letr letr_count words_count i pos prev_index prev_div)
-                    div (first result)
-                    letr_index (second result)
-                    new_word (conj word (get letters letr_index))]
-                (recur new_word i (inc pos) res letr_index div))))))
+  (if (<= length 0)
+    '()
+    (permuteAll alphabet alphabet length)))
 
 ;1.3
 (defn my-map [f coll]
   (reduce (fn [acc item]
-            (cons item acc)) ; reverse
-    '()
-    (reduce (fn [acc item]
-              (cons (f item) acc))
-      '()
-            coll)))
-
+            (concat acc [(f item)]))
+          '()
+          coll))
 
 (defn my-filter [pred coll]
   (reduce (fn [acc item]
-            (cons item acc)) ; reverse
-    '()
-    (reduce (fn [acc item]
-              (if (pred item) (cons item acc)
-                           acc))
-      '()
-            coll)))
-
+            (concat acc (if (pred item) [item] [])))
+          '()
+          coll))
 
 ;1.4
 (defn permute2 [alphabet length]
-  (if (<= length 0) ()
-                    (let [contains (fn [item l]
-                                     (reduce (fn [acc cur]
-                                               (if (= item cur) true
-                                                                acc))
-                                             false l))
-                          builder (fn [oldList] (reduce (fn [result item]
-                                                          (concat item result))
-                                                        ()
-                                                        (map (fn [oldWord]
-                                                               (reduce (fn [acc letter]
-                                                                         (if (= letter (first oldWord)) acc
-                                                                                                        (cons (cons letter oldWord) acc)))
-                                                                       ()
-                                                                       alphabet))
-                                                             oldList)))]
-                      (map (fn [x]
-                             (apply str x)) ;(a b c) => abc
-                           (reduce (fn [acc i]
-                                     (builder acc))
-                                   '(())
-                                   (range length))))))
+  (if (<= length 0)
+    '()
+    (let [builder (fn [oldList]
+                    (reduce (fn [result item]
+                              (concat item result))
+                            '()
+                            (my-map (fn [oldWord]
+                                      (reduce (fn [acc letter]
+                                                (if (= letter (first oldWord))
+                                                  acc
+                                                  (cons (cons letter oldWord) acc)))
+                                              '()
+                                              alphabet))
+                                    oldList)))]
+      (my-map (fn [x]
+                (apply str x))
+              (reduce (fn [acc i]
+                        (builder acc))
+                      '(())
+                      (range length))))))
--- task1/test/task1/core_test.clj
+++ task1/test/task1/core_test.clj
@@ -17,7 +17,7 @@
 
 (deftest given-test-task1-permute
   (testing "Тест из задачи"
-    (is (= (permute '("a" "b" "c") 2) '(["a" "b"] ["a" "c"] ["b" "a"] ["b" "c"] ["c" "a"] ["c" "b"])))))
+    (is (= (permute '("a" "b" "c") 2) '("bc" "ac" "cb" "ab" "ca" "ba")))))
 
 (deftest test-cons1-task1-permute
   (testing "'(a) 2"
@@ -29,32 +29,32 @@
 
 (deftest test-cons3-task1-permute
   (testing "'(a b) 2"
-    (is (= (permute '(a b) 2) '([a b] [b a])))))
+    (is (= (permute '(a b) 2) '("ab" "ba")))))
 
 (deftest test-num-vec-task1-permute
   (testing "Вектор с числами"
-    (is (= (permute [3 2 1] 3) '([1 3 1] [1 3 2] [1 2 1] [1 2 3] [3 1 3] [3 1 2] [3 2 1] [3 2 3] [2 1 3] [2 1 2] [2 3 1] [2 3 2])))))
+    (is (= (permute [3 2 1] 3) '("123" "223" "323" "113" "213" "313" "132" "232" "332" "112" "212" "312" "131" "231" "331" "121" "221" "321")))))
 
 (deftest test-number-task1-permute
   (testing "Работа с числами"
-    (is (= (permute [32 1] 2) '([1 32] [32 1])))))
+    (is (= (permute [32 1] 2) '("321" "132")))))
 
 ;;Тесты для 1.3
 (deftest test-range6-task1-my-map
   (testing "range 6"
-    (is (= (println (map (fn [x] (* x x)) (range 6))) (println (my-map (fn [x] (* x x)) (range 6)))))))
+    (is (= (map (fn [x] (* x x)) (range 6)) (my-map (fn [x] (* x x)) (range 6))))))
 
 (deftest test-inc-task1-my-map
   (testing "inc"
-    (is (= (println (map inc [1 2 3 4 5])) (println (my-map inc [1 2 3 4 5]))))))
+    (is (= (map inc [1 2 3 4 5]) (my-map inc [1 2 3 4 5])))))
 
 (deftest test-range6-even-task1-my-filter
   (testing "Четные числа range 6"
-    (is (= (println (filter even? (range 6))) (println (my-filter even? (range 6)))))))
+    (is (= (filter even? (range 6)) (my-filter even? (range 6))))))
 
 (deftest test-range121-mod3-task1-my-filter
   (testing "Из лекции"
-    (is (= (println (filter (fn [n] (= 0 (mod n 3))) (range 1 21))) (println (my-filter (fn [n] (= 0 (mod n 3))) (range 1 21)))))))
+    (is (= (filter (fn [n] (= 0 (mod n 3))) (range 1 21)) (my-filter (fn [n] (= 0 (mod n 3))) (range 1 21))))))
 
 ;Тесты для задачи 1.4
 (deftest empty-test-task1-permute2
